8ed07c1919b1c5a8928df55ccbdcc52d5fc0e653
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-04-27 17:41:37 -0400

    Add toolbar menu

--- app/src/main/java/com/example/preferencestoyapp/MainActivity.java
+++ app/src/main/java/com/example/preferencestoyapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.preferencestoyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.preferencestoyapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setSupportActionBar(binding.toolbar);
+    }
+
+    /**
+     * @return true to show the menu - false to not show it.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_preferences, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.preferences:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
