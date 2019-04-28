eb1477cda85cf6dcccdedf638efc27aaa65df0d2
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-04-28 18:04:02 -0400

    Add button to retrieve Sync preference
    You have to manually retrieve the value by pressing the button because
    a change listener does not work - the listener in MainActivity is paused
    when PreferenceActivity is started (duh).

--- app/src/main/java/com/example/preferencestoyapp/MainActivity.java
+++ app/src/main/java/com/example/preferencestoyapp/MainActivity.java
@@ -2,26 +2,41 @@ package com.example.preferencestoyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.preference.PreferenceManager;
 
 import com.example.preferencestoyapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setSupportActionBar(binding.toolbar);
+
+        binding.buttonGetSync.setOnClickListener(view -> {
+            boolean prefsBoolean = PreferenceManager
+                    .getDefaultSharedPreferences(this)
+                    .getBoolean(getString(R.string.perf_sync_checkbox_key), false);
+
+            binding.textViewSharedPrefs.setText(String.valueOf(prefsBoolean));
+        });
+
 
         // This sets default values for your preferences by reading preference XML
         // file - it use each's `defaultValue` and `key` attributes to do so.
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        initPoorMansPrefListener();
+    }
 
+    private void initPoorMansPrefListener() {
         // This only works because this Activity IS NOT `singleTop`, thus it is re-created
         // every time it is accessed - even when going up/back.
         boolean switchState = PreferenceManager
@@ -29,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 .getBoolean(getString(R.string.perf_switch_key), false);
         binding.textView.setText(String.valueOf(switchState));
     }
+
 
     /**
      * @return true to show the menu - false to not show it.
